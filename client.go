7f11c3c0d4290f202bec2a990270509e50d90e98
Scott <morsescott88@gmail.com>
2020-08-07 08:23:04 +0100

    Revert "Update to client and client test"
    This reverts commit 64310e08bd682b587585aae6181a1861f216f066.

--- client.go
+++ client.go
@@ -1,16 +1,11 @@
 package redis
 
 import (
-	"context"
 	"errors"
-	"github.com/ONSdigital/log.go/log"
 	"time"
 
-	"github.com/ONSdigital/dp-sessions-api/session"
 	goredis "github.com/go-redis/redis"
 )
-
-var ctx = context.Background()
 
 // Client - structure for the redis client
 type Client struct {
@@ -44,26 +39,4 @@ func NewClient(c Config) (*Client, error) {
 		}),
 		ttl: c.TTL,
 	}, nil
-}
-
-//Set add session to redis
-func (c *Client) Set(s *session.Session) error {
-	if s == nil {
-		log.Event(ctx, "session is empty", log.ERROR)
-		return errors.New("session is empty")
-	}
-
-	json, err := s.MarshalJSON()
-	if err != nil {
-		log.Event(ctx, "failed to marshal session", log.Error(err), log.ERROR)
-		return err
-	}
-
-	msg, err := c.client.Set(s.ID, json, c.ttl).Result()
-	if err != nil {
-		log.Event(ctx, msg, log.Error(err), log.ERROR)
-	}
-	log.Event(ctx, msg, log.INFO)
-
-	return nil
 }
