ba8b4aa6a4ff382531563141c9181f53d9ef675a
Scott <morsescott88@gmail.com>
2020-08-10 09:35:26 +0100

    Added tests for setting up new redis client

--- client_test.go
+++ client_test.go
@@ -1,22 +1,52 @@
 package redis
 
 import (
-	"testing"
-
 	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
 )
 
+func TestNewClient(t *testing.T) {
+	Convey("Given correct redis config", t, func() {
+		c, err := setUpClient("123.0.0.1", "1234", 0, 0)
 
-func TestClient_Set(t *testing.T) {
-	Convey("Given redis client is set up", t, func() {
-
-
-		Convey("When a valid session is provided", func() {
-
-
-			Convey("Then the session is stored in redis", func() {
-
-			})
+		Convey("Then the client will be created", func() {
+			So(err, ShouldBeNil)
+			So(c, ShouldNotBeEmpty)
 		})
 	})
+
+	Convey("Given redis config with missing address", t, func() {
+		c, err := setUpClient("", "1234", 0, 0)
+
+		Convey("Then the client will fail to be created", func() {
+			So(c, ShouldBeNil)
+			So(err, ShouldNotBeEmpty)
+			So(err.Error(), ShouldEqual, "address is missing")
+		})
+	})
+
+	Convey("Given redis config with missing password", t, func() {
+		c, err := setUpClient("123.0.0.1", "", 0, 0)
+
+		Convey("Then the client will fail to be created", func() {
+			So(c, ShouldBeNil)
+			So(err, ShouldNotBeEmpty)
+			So(err.Error(), ShouldEqual, "password is missing")
+		})
+	})
+}
+
+func TestClient_Set(t *testing.T) {
+
+}
+
+func setUpClient(addr, password string, database int, ttl time.Duration) (*Client, error){
+	c, err := NewClient(Config{
+		Addr:     addr,
+		Password: password,
+		Database: database,
+		TTL:      ttl,
+	})
+	return c, err
 }
